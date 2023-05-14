6c159a0b7d84866208cf6d1ca40dd123319a36e8
christopherdongo <chris_d26@hotmail.com>
2023-05-14 09:36:49 -0500

    update config db

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -11,13 +11,13 @@ const options = {};
 let client;
 let clientPromise: Promise<MongoClient>;
 
-let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise: Promise<MongoClient>
-  }
-
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise: Promise<MongoClient>
+  }
+  
   if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
     globalWithMongo._mongoClientPromise = client.connect()
