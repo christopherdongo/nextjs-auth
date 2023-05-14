c6caec84d03d6fe4bd682aabf105a50115347102
christopherdongo <chris_d26@hotmail.com>
2023-05-14 02:53:32 -0500

    update config mongo client

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -8,17 +8,21 @@ if (!process.env.DATABASE_URL) {
 const uri = process.env.DATABASE_URL;
 const options = {};
 
-let client:MongoClient;
+let client;
 let clientPromise: Promise<MongoClient>;
+
+let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise: Promise<MongoClient>
+  }
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
